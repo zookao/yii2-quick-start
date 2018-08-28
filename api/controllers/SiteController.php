5d364b6299257c99f802381b50a90901da8b1a07
czc <caozongchao@163.com>
2018-08-28 11:49:00 +0800

    增加api目录，可在这里编写restful接口

---
+++ api/controllers/SiteController.php
@@ -0,0 +1,22 @@
+<?php
+namespace api\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
